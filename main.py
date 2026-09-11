919757ac33ac6a9378a812305b975b5e4cf02701
HosseinYasinzadehh <hosseinyasinzadeh808@gmail.com>
2026-09-11 13:07:24 +0330

    feat: add quiz question loading and shuffling

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import json
+import random
 
 def load_questions():
     with open("questions.json") as file:
         result = json.load(file)
-
     return result
 
-questions = load_questions()
-print(questions)
+def run_quiz(questions):
+    random.shuffle(questions)
+    for quiz in questions:
+        print(quiz['question'])
+        for option in quiz['options']:
+            print(option)
