cc7f7e937e0f133f65da122aa2e6ad6ce5304737
HosseinYasinzadehh <hosseinyasinzadeh808@gmail.com>
2026-09-13 08:10:23 +0330

    feat: add quiz logic and scoring

--- main.py
+++ main.py
@@ -6,9 +6,39 @@ def load_questions():
         result = json.load(file)
     return result
 
+
+def ask_question(quiz):
+    choice = 1
+    print(quiz['question'])
+    for option in quiz['options']:
+        print(f"{choice}. {option}")
+        choice += 1
+    while True:
+        try:
+            user_choice = int(input("please enter your choice: "))
+            if user_choice > 4 or user_choice < 1:
+                print("please enter number between 1 to 4!!!")
+                continue
+            print(f"your choice is {user_choice}")
+            if user_choice == quiz["answer"]:
+                print("your choice is correct")
+                return True
+            else:
+                print("your choice is not correct")
+                return False
+
+        except ValueError:
+            print("please enter number between 1 to 4!!!")
+
 def run_quiz(questions):
     random.shuffle(questions)
+    score = 0
     for quiz in questions:
-        print(quiz['question'])
-        for option in quiz['options']:
-            print(option)
+       result = ask_question(quiz)
+       if result:
+           score += 1
+    return score
+
+questions = load_questions()
+score = run_quiz(questions)
+print(score)
